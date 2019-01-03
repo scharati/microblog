16ca7208aac282b5a7b21389a119e1cf12f8ed0f
scharati shailesh.charati@gmail.com <shailesh.charati@gmail.com>
2019-01-03 07:39:15 +0000

    templates - loops

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,15 @@ from flask import render_template
 @app.route('/index')
 def index():
     user = {"username" : 'Shailesh'}
-    return render_template("index.html", user = user, title = "Namaste")
+    posts = [
+                {
+                    'author': {'username': 'Shailesh'},
+                    'body' : '2019 - the panipat of 21st century'
+                },
+                
+                {   
+                    'author': {'username' : 'Dhriti'},
+                    'body': 'Chinnara kathegalu'
+                }
+            ]
+    return render_template("index.html", user = user, title = "Namaste", posts = posts)
