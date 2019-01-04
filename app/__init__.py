f29a56097ca0df348b9b7d03b55564b1ff613d45
scharati shailesh.charati@gmail.com <shailesh.charati@gmail.com>
2019-01-04 09:46:05 +0000

    database changes - flask-sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,13 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-from app import routes
+app.config.from_object(Config)
+# create the db interface [flask-sqlalchemy]
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -17,3 +18,12 @@ def index():
                 }
             ]
     return render_template("index.html", user = user, title = "Namaste", posts = posts)
+
+@app.route('/login' , methods = ["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash( 'Login requested for user {}, remember_me={}'.format( form.username.data, form.remember_me.data ) )
+        return redirect( url_for("index") )
+    else:
+        return render_template('login.html', title='Sign In', form = form)
